6092dec9c28110be12c5b0914819ab2b8e894a78
Ashish Singh <singhashish19031998@icloud.com>
2018-06-24 12:34:43 +0530

    Sqltemplate resolver is working fine

---
+++ test/models/sql_template_test.rb
@@ -0,0 +1,30 @@
+require 'test_helper'
+
+class SqlTemplateTest < ActiveSupport::TestCase
+
+  test "resolver returns a template with the saved body" do
+    resolver = SqlTemplate::Resolver.new
+    details = { formats: [:html], locale: [:en], handlers: [:erb] }
+
+    assert resolver.find_all("index", "posts", false, details ).empty?
+    SqlTemplate.create!(
+                   body: "<%= 'Hi form SqlTemplate' %>",
+                   path: "posts/index",
+                   format: "html",
+                   locale: "en",
+                   handler: "erb",
+                   partial: false
+                   )
+    # Assertion about template can be found
+    template = resolver.find_all("index","posts",false,details).first
+    assert_kind_of ActionView::Template, template
+
+    #Assertion about specific information about the template
+    assert_equal "<%= 'Hi form SqlTemplate' %>", template.source
+    assert_kind_of ActionView::Template::Handlers::ERB, template.handler
+    assert_equal [:html], template.formats
+    assert_equal "posts/index", template.virtual_path
+    assert_match %r[SqlTemplate - \d+ - "posts/index"], template.identifier
+  end
+
+end
